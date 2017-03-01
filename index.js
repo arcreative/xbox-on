d6b6d98372248d5c77ea461649e3fe111b1c410a
Roy Kokkelkoren <roy.kokkelkoren@gmail.com>
2017-03-01 21:50:25 +0100

    Modified sending of PowerON UDP pkt to 5 times

--- index.js
+++ index.js
@@ -13,19 +13,31 @@ function Xbox(ip, id) {
 Xbox.prototype.powerOn = function(callback) {
   callback = callback || function() {};
 
-  // Open socket
-  var socket = dgram.createSocket('udp4');
+  var counter = 0,
+      id = this.id,
+      ip = this.ip,
+      socket = dgram.createSocket('udp4');
+
+  var timeId = setInterval(function() {
+      Xbox.prototype.sendOn(ip, id, socket, counter, timeId, callback);
+      counter++;
+    }, 1000);
+};
+
+Xbox.prototype.sendOn = function(ip, id, socket, counter, timeId, callback) {
 
   // Create payload
-  var powerPayload = new Buffer('\x00' + String.fromCharCode(this.id.length) + this.id + '\x00'),
+  var powerPayload = new Buffer('\x00' + String.fromCharCode(id.length) + id + '\x00'),
       powerPayloadLength = new Buffer(String.fromCharCode(powerPayload.length)),
       powerHeader = Buffer.concat([new Buffer('dd0200', 'hex'), powerPayloadLength, new Buffer('0000', 'hex')]),
       powerPacket = Buffer.concat([powerHeader, powerPayload]);
 
-  // Send
-  socket.send(powerPacket, 0, powerPacket.length, PORT, this.ip, function(err) {
-    if (err) return callback(err);
-    socket.close();
-    callback();
+  socket.send(powerPacket, 0, powerPacket.length, PORT, ip, function(err) {
+    if(err){
+      clearInterval(timeId);
+      return callback(err);
+    } else if(counter == 5){
+      return callback();
+    }
   });
 };
